0ec30a84bce186f1de853d6070b9e9bb9087fbeb
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2025-01-01 16:06:46 -0600

    serve font as static asset

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,13 @@
 import { MainScreen } from "./components/MainScreen";
+import { StaticJetBrainsMono } from "./components/StaticJetBrainsMono";
 
 const App = () => {
-  return <MainScreen />;
+  return (
+    <>
+      <StaticJetBrainsMono />
+      <MainScreen />
+    </>
+  );
 };
 
 export default App;
