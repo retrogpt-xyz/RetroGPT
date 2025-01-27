462eb110ca9d5997fc16f3bf9f8109034bdaeaa0
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2025-01-27 00:33:48 -0600

    migrate to streaming response types

--- backend/server/endpoint/api_prompt.rs
+++ backend/server/endpoint/api_prompt.rs
@@ -1,5 +1,11 @@
+use std::convert::identity;
+
 use http_body_util::BodyExt;
-use hyper::{body::Body, header::CONTENT_TYPE, Response, StatusCode};
+use hyper::{
+    body::{Body, Bytes, Frame},
+    header::CONTENT_TYPE,
+    Response, StatusCode,
+};
 use serde_json::json;
 
 use crate::{
@@ -7,43 +13,43 @@ use crate::{
     db::chats::{add_to_chat, get_chat_by_id},
     gpt::gpt_api,
     server::{
-        error::{bad_request, internal_error},
-        form_body, IncReqst, RGptResp,
+        error::{error_400, error_500},
+        IncReqst, OutResp,
     },
 };
 
-pub async fn api_prompt(cfg: &Cfg, req: IncReqst) -> RGptResp {
-    api_prompt_inner(cfg, req).await.unwrap_or_else(|x| x)
+pub async fn api_prompt(cfg: &Cfg, req: IncReqst) -> OutResp {
+    api_prompt_inner(cfg, req).await.unwrap_or_else(identity)
 }
 
-pub async fn api_prompt_inner(cfg: &Cfg, req: IncReqst) -> Result<RGptResp, RGptResp> {
+pub async fn api_prompt_inner(cfg: &Cfg, req: IncReqst) -> Result<OutResp, OutResp> {
     if req.body().size_hint().upper().unwrap_or(u64::MAX) > cfg.max_req_size {
-        return Err(bad_request("request body is too large"));
+        return Err(error_400("request body is too large"));
     }
 
     let bytes = req
         .collect()
         .await
-        .map_err(|_| internal_error())?
+        .map_err(|_| error_500())?
         .to_bytes()
         .to_vec();
-    let prompt = std::str::from_utf8(&bytes).map_err(|_| internal_error())?;
+    let prompt = std::str::from_utf8(&bytes).map_err(|_| error_500())?;
 
-    let (recvd, new_head_id, chat_id) = gpt_api(cfg, prompt).await.map_err(|_| internal_error())?;
-    let parsed = serde_json::from_str::<serde_json::Value>(&recvd).map_err(|_| internal_error())?;
+    let (recvd, new_head_id, chat_id) = gpt_api(cfg, prompt).await.map_err(|_| error_500())?;
+    let parsed = serde_json::from_str::<serde_json::Value>(&recvd).map_err(|_| error_500())?;
 
     let resp = match parsed
         .get("choices")
-        .ok_or_else(internal_error)?
+        .ok_or_else(error_500)?
         .get(0)
-        .ok_or_else(internal_error)?
+        .ok_or_else(error_500)?
         .get("message")
-        .ok_or_else(internal_error)?
+        .ok_or_else(error_500)?
         .get("content")
-        .ok_or_else(internal_error)?
+        .ok_or_else(error_500)?
     {
         serde_json::Value::String(string) => String::from(string),
-        _ => Err(internal_error())?,
+        _ => Err(error_500())?,
     };
 
     let mut conn = cfg.db_conn.lock().await;
@@ -59,9 +65,13 @@ pub async fn api_prompt_inner(cfg: &Cfg, req: IncReqst) -> Result<RGptResp, RGpt
         "chatId": chat_id,
     });
 
+    let stream = Box::pin(futures::stream::once(async move {
+        Ok(Frame::data(Bytes::from(be_req_msg.to_string())))
+    }));
+
     Response::builder()
         .status(StatusCode::OK)
         .header(CONTENT_TYPE, "application/json")
-        .body(form_body(be_req_msg.to_string()))
-        .map_err(|_| internal_error())
+        .body(crate::server::form_stream_body(stream))
+        .map_err(|_| error_500())
 }
--- backend/server/mod.rs
+++ backend/server/mod.rs
@@ -5,16 +5,25 @@ pub mod predicate;
 use crate::cfg::Cfg;
 
 use std::error::Error;
+use std::io;
 use std::sync::Arc;
 use std::{convert::Infallible, net::SocketAddr};
 
-use http_body_util::Full;
+use futures::Stream;
+use http_body_util::{Full, StreamBody};
+use hyper::body::Frame;
 use hyper::{body::Bytes, server::conn::http1, service::service_fn, Request, Response};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
 pub type IncReqst = Request<hyper::body::Incoming>;
+#[deprecated]
 pub type RGptResp = Response<Full<Bytes>>;
+pub type OutResp = Response<
+    StreamBody<
+        Box<dyn Unpin + Send + futures::Stream<Item = Result<Frame<Bytes>, std::io::Error>>>,
+    >,
+>;
 
 #[macro_export]
 macro_rules! handle_endpoint {
@@ -25,18 +34,26 @@ macro_rules! handle_endpoint {
     };
 }
 
-pub async fn handle_request(cfg: Arc<Cfg>, req: IncReqst) -> Result<RGptResp, Infallible> {
+pub async fn handle_request(cfg: Arc<Cfg>, req: IncReqst) -> Result<OutResp, Infallible> {
     println!("{}", req.uri().path());
 
     handle_endpoint!(predicate::api_prompt, endpoint::api_prompt, cfg, req);
     handle_endpoint!(predicate::serve_static, endpoint::serve_static, cfg, req);
-    handle_endpoint!(predicate::api_def_sess, endpoint::api_def_sess, cfg, req);
+    // handle_endpoint!(predicate::api_def_sess, endpoint::api_def_sess, cfg, req);
 
-    Ok(error::bad_request("request did not match any endpoints"))
+    Ok(error::error_400("request did not match any endpoints"))
 }
 
-fn form_body<B: Into<Bytes>>(bytes: B) -> Full<Bytes> {
-    Full::new(bytes.into())
+fn form_stream_body<S>(
+    stream: S,
+) -> StreamBody<Box<dyn Send + Unpin + 'static + Stream<Item = Result<Frame<Bytes>, io::Error>>>>
+where
+    S: Stream<Item = Result<Frame<Bytes>, io::Error>> + Unpin + Send + 'static,
+{
+    StreamBody::new(Box::new(stream)
+        as Box<
+            dyn Unpin + Send + Stream<Item = Result<Frame<Bytes>, std::io::Error>>,
+        >)
 }
 
 pub async fn run_server() -> Result<(), Box<dyn Error>> {
