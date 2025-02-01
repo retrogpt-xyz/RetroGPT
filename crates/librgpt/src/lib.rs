339f2b8915bd954ae04751d94d4c576a9e49c274
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2025-01-31 19:39:20 -0600

    document entrypoint

--- crates/librgpt/src/lib.rs
+++ crates/librgpt/src/lib.rs
@@ -1,13 +1,16 @@
+/// Main logic for RetroGPT
 pub mod cfg;
 pub mod db;
 pub mod gpt;
 pub mod server;
 pub mod startup;
 
-pub fn run_app() -> Result<(), Box<dyn std::error::Error>> {
-    startup::startup();
-
-    let rt = tokio::runtime::Runtime::new()?;
-
-    rt.block_on(async { server::run_server().await })
+/// The main entrypoint for the application
+///
+/// Invoked by the binary crate.
+#[tokio::main]
+pub async fn run_app() -> Result<(), Box<dyn std::error::Error>> {
+    // Run startup logic before starting the backend server
+    tokio::task::spawn_blocking(startup::startup).await?;
+    server::run_server().await
 }
