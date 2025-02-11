eacdc127b2e0add085af6361f9d051036dd84bce
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2025-02-11 00:06:50 -0600

    formatting

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -6,7 +6,11 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-  <GoogleOAuthProvider clientId={"262296442193-ctnfmo1iedrcj8q1luvn2724r7q8gkel.apps.googleusercontent.com"}>
+  <GoogleOAuthProvider
+    clientId={
+      "262296442193-ctnfmo1iedrcj8q1luvn2724r7q8gkel.apps.googleusercontent.com"
+    }
+  >
     <StrictMode>
       <App />
     </StrictMode>
