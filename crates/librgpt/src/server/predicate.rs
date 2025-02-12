f022a6b7196b6a270632a1e5f21f3b41bf41699e
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2025-02-12 16:10:05 -0600

    remove predicates

--- crates/librgpt/src/server/predicate.rs
+++ crates/librgpt/src/server/predicate.rs
@@ -5,24 +5,8 @@ pub async fn api_prompt(_cfg: &Cfg, req: &IncReqst) -> bool {
     req.uri().path().starts_with("/api/prompt")
 }
 
-pub async fn api_def_sess(_cfg: &Cfg, req: &IncReqst) -> bool {
-    req.uri().path().starts_with("/api/get_def_sess")
-}
-
-pub async fn serve_static(cfg: &Cfg, req: &IncReqst) -> bool {
-    let mut path = cfg.static_dir.join(&req.uri().path()[1..]);
-    if path.is_dir() {
-        path = path.join("index.html");
-    }
-    path.is_file()
-}
-
 pub async fn auth(_cfg: &Cfg, req: &IncReqst) -> bool {
     req.uri().path().starts_with("/api/auth")
-}
-
-pub async fn session(_cfg: &Cfg, req: &IncReqst) -> bool {
-    req.uri().path().starts_with("/api/session")
 }
 
 pub async fn api_chats(_cfg: &Cfg, req: &IncReqst) -> bool {
