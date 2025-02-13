6271dba6ce425214d2f935de01dd9c1cf7d48f21
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2025-02-12 19:56:47 -0600

    .

--- crates/librgpt/src/server/predicate.rs
+++ crates/librgpt/src/server/predicate.rs
@@ -5,10 +5,6 @@ pub async fn api_prompt(_cfg: &Cfg, req: &IncReqst) -> bool {
     req.uri().path().starts_with("/api/prompt")
 }
 
-pub async fn api_chats(_cfg: &Cfg, req: &IncReqst) -> bool {
-    req.uri().path().starts_with("/api/chats")
-}
-
 pub async fn api_chat_messages(_cfg: &Cfg, req: &IncReqst) -> bool {
     req.uri().path() == "/api/chat/messages"
 }
