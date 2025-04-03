f3d692e8bc5539cc6331fc002040baf3fb64d4bf
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2025-04-03 10:26:37 -0500

    better base url provide layer

--- frontend/BaseUrl.ts
+++ frontend/BaseUrl.ts
@@ -3,31 +3,33 @@ import { Context, Effect, Layer } from "effect";
 import * as WindowLocation from "./WindowLocation";
 import { IllegalArgumentException } from "effect/Cause";
 
-const viteBaseUrl = Url.fromString("http://localhost:4002");
-const composeBaseUrl = Effect.gen(function* () {
+export const getViteBaseUrl = Url.fromString("http://localhost:4002");
+export const getComposeBaseUrl = Effect.gen(function* () {
   const location = yield* WindowLocation.WindowLocation;
   return yield* Url.fromString(yield* location.origin);
-}).pipe(Effect.provide(WindowLocation.layer));
+});
 
-export class BaseUrlProvider extends Context.Tag("tag")<
+export class BaseUrlProvider extends Context.Tag("@rgpt/BaseUrlProvider")<
   BaseUrlProvider,
   {
     get: Effect.Effect<URL, IllegalArgumentException>;
   }
 >() {}
 
-const providerBuilder = (
-  getter: Effect.Effect<URL, IllegalArgumentException>,
-) => {
-  Layer.succeed(
-    BaseUrlProvider,
-    BaseUrlProvider.of({
-      get: getter,
-    }),
-  );
-};
+export const isVite = Effect.sync(() => import.meta.env.DEV);
 
-export const layers = {
-  viteBaseLayer: providerBuilder(viteBaseUrl),
-  composeBaseLayer: providerBuilder(composeBaseUrl),
-};
+export const layer = Layer.effect(
+  BaseUrlProvider,
+  Effect.gen(function* () {
+    const getter = (yield* isVite)
+      ? getViteBaseUrl
+      : getComposeBaseUrl.pipe(
+          Effect.provideService(
+            WindowLocation.WindowLocation,
+            yield* WindowLocation.WindowLocation,
+          ),
+        );
+
+    return { get: getter };
+  }),
+);
